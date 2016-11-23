b3a0c1f294281ed5a8b1128af569d1aad3326ff5
HasanMehadi <mehadi.hasan.uiu@gmail.com>
2016-11-24 02:44:07 +0600

    added Update and Delete view. Song Part Started

--- music/urls.py
+++ music/urls.py
@@ -11,11 +11,16 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9])+$', views.DetailView.as_view(), name="detail"),
 
     #Add New Album To the Database
-    url(r'^album/add/$', views.AlbumCreate.as_view(), name='add_album')
+    url(r'^album/add/$', views.AlbumCreate.as_view(), name='add_album'),
 
 
     # music/id/favourite
     #url(r'^(?P<album_id>[0-9])+/favourite/$', views.favourite, name="favourite"),
 
+    # Update Album using primary key
+    url(r'^album/(?P<pk>[0-9])+/$', views.AlbumUpdate.as_view(), name='update_album'),
+
+    # Delete Album using primary key and redirect to home page
+    url(r'^album/(?P<pk>[0-9])+/delete/$', views.AlbumDelete.as_view(), name='delete_album')
 ]
 
--- music/views.py
+++ music/views.py
@@ -35,7 +35,8 @@ def favourite(request, album_id):
 
 from django.views import generic
 from .models import Album
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
 
 
 class IndexView(generic.ListView):
@@ -43,7 +44,7 @@ class IndexView(generic.ListView):
     template_name = 'music/index.html'
 
     def get_queryset(self):
-       return Album.objects.all()
+        return Album.objects.all()
 
 
 class DetailView(generic.DetailView):
@@ -54,3 +55,13 @@ class DetailView(generic.DetailView):
 class AlbumCreate(CreateView):
     model = Album
     fields = ['artist', 'album_title', 'genre', 'album_logo', 'is_favorite']
+
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo', 'is_favorite']
+
+
+class AlbumDelete(DeleteView):
+    model = Album
+    success_url = reverse_lazy('music:index')
